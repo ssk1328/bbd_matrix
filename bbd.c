42e30749ba6c743119cd32031e6431777eb5ed23
ssk1328 <shashankgangrade@gmail.com>
2016-09-14 13:23:49 +0530

    API gives all Xi values now

--- bbd.c
+++ bbd.c
@@ -49,45 +49,42 @@ void matrix_invm (int size, double * M, double * N, double * R) {
 
 }
 
-void matrix_inv_m (int size, double * M, double * B, double * C, double * D) {
+void matrix_inv_m (int size, double * A, double * C, double * B, double * G, double * tempB, double * tempG) {
 
 	//	Agruments:
 	//	size: 	int - Size of matrix
-	// 	M: 		pointer to double - Pointer to start of Matrix M, stored in contagious array
-	// 	B: 		pointer to double - Pointer to start of Matrix B, stored in contagious array
-	// 	C: 		pointer to double - Pointer to start of Matrix C, stored in contagious array
-	// 	D: 		pointer to double - Pointer to start of Matrix D, stored in contagious array
+	// 	A: 		pointer to double - Pointer to start of Matrix M, stored in contagious array
+	// 	C: 		pointer to double - Pointer to start of Matrix B, stored in contagious array
+	// 	B: 		pointer to double - Pointer to start of Matrix C, stored in contagious array
+	// 	G: 		pointer to double - Pointer to start of Matrix D, stored in contagious array
 
-	// C = inv(M)*B*C
-	// D = inv(M)*B*D
+	// tempB  = C*inv(A)*B
+	// tempG  = C*inv(A)*G
 
 	int elements = size*size;
 	int pivotArray[size];
 	int errorHandler;
 	// TODO: Change this to malloc, not a good idea when elements is a large integer
 	double lapackWorkspace0[elements];
-	double lapackWorkspace1[elements];
 
 	char TRANS = 'N';
 	double ALPHA = 1.0;
 	double BETA = 0.0;
 
 	// matrix inversion
-	// Inverted matrix in again stored in M
-	dgetrf_(&size, &size, M, &size, pivotArray, &errorHandler);
-	dgetri_(&size, M, &size, pivotArray, lapackWorkspace0, &elements, &errorHandler);	
+	// Inverted matrix in again stored in A
+	dgetrf_(&size, &size, A, &size, pivotArray, &errorHandler);
+	dgetri_(&size, A, &size, pivotArray, lapackWorkspace0, &elements, &errorHandler);	
 
-	// matrix multiplication lapackworkspace0 = B*C
-    dgemm_(&TRANS, &TRANS, &size, &size, &size, &ALPHA, B, &size, C, &size, &BETA, lapackWorkspace0, &size);
+	// matrix multiplication lapackworkspace0 = C*inv(A)
+    dgemm_(&TRANS, &TRANS, &size, &size, &size, &ALPHA, C, &size, A, &size, &BETA, lapackWorkspace0, &size);
 
-	// matrix multiplication lapackworkspace1 = B*D
-    dgemm_(&TRANS, &TRANS, &size, &size, &size, &ALPHA, B, &size, D, &size, &BETA, lapackWorkspace1, &size);
+	// matrix multiplication tempB = lapackworkspace0*B
+    dgemm_(&TRANS, &TRANS, &size, &size, &size, &ALPHA, lapackWorkspace0, &size, B, &size, &BETA, tempB, &size);
 
-	// matrix multiplication C = M*lapackworkspace0
-    dgemm_(&TRANS, &TRANS, &size, &size, &size, &ALPHA, M, &size, lapackWorkspace0, &size, &BETA, C, &size);
-
-	// matrix multiplication D = M*lapackworkspace1
-    dgemm_(&TRANS, &TRANS, &size, &size, &size, &ALPHA, M, &size, lapackWorkspace1, &size, &BETA, D, &size);
+    // TODO: Replace this with matrix vector multiplication
+	// matrix multiplication tempG = lapackworkspace0*G
+    dgemm_(&TRANS, &TRANS, &size, &size, &size, &ALPHA, lapackWorkspace0, &size, G, &size, &BETA, tempG, &size);
 
 }
 
@@ -102,6 +99,30 @@ void matrix_add(int size, double * A, double * B, double scalar)
 	}
 }
 
+void matrix_xi(int size, double * A, double * B, double * G, double * Xn, double * Xi)
+{
+	// Xi = A*G - A*B*Xn		
+	int elements = size*size;
+	char TRANS = 'N';
+	double ALPHA = 1.0;
+	double BETA = 0.0;
+	// TODO: Change this to malloc 
+	double lapackWorkspace1[elements];
+	double lapackWorkspace2[elements];
+
+	// matrix multiplication Xi = A*G
+    dgemm_(&TRANS, &TRANS, &size, &size, &size, &ALPHA, A, &size, G, &size, &BETA, Xi, &size);
+
+	// matrix multiplication lapackworkspace1 = A*B
+    dgemm_(&TRANS, &TRANS, &size, &size, &size, &ALPHA, A, &size, B, &size, &BETA, lapackWorkspace1, &size);
+
+	// matrix multiplication lapackworkspace2 = lapackworkspace1*Xn
+    dgemm_(&TRANS, &TRANS, &size, &size, &size, &ALPHA, lapackWorkspace1, &size, Xn, &size, &BETA, lapackWorkspace2, &size);
+
+    // Xi = Xi - lapackworkspace2
+    matrix_add(size, Xi, lapackWorkspace2, -1.0);
+}
+
 struct Matrix {
 	// row first form 
 	// Each matrix can have max size of 10x10
@@ -110,62 +131,62 @@ struct Matrix {
 	double matrix[100];
 };
 
-void solve_bbd( int 	nMat, 	
-				struct 	Matrix * matA, 
-				struct 	Matrix * matB, 
-				struct 	Matrix * matC, 
-				struct 	Matrix * matG,
-				struct 	Matrix * matX) 
-{
+void solve_bbd( int nMat, struct Matrix * matA, struct Matrix * matB, struct Matrix * matC, struct 	Matrix * matG, struct 	Matrix * matX) {
 /*
 
-	A 		    C 	X 		G
-	   A 	    C 	X 		G
-	      A     C *	X 	= 	G
-		     A	C 	X 		G
-	B  B  B  B  A 	X 		G
+	A 		   B   X   G
+	   A 	   B   X   G
+	      A    B * X = G
+		     A B   X   G
+	C  C  C  C A   X   G
 
 	Arguments:
 	nMat: 	Number of diagonal blocks minus one (nMat = 4 in above example)
 	matA:	Pointer to array of Struct Matrix A, diagonal matrix blocks
 	matB:	Pointer to array of Struct Matrix B, bottom border matrix blocks
 	matC:	Pointer to array of Struct Matrix C, right border matrix blocks
-	matG:	Pointer to array of Struct Matrix G, diagonal matrix
-	matX:	Pointer to array of Struct Matrix X, Blocks of Solution matrix
+	matG:	Pointer to array of Struct Matrix G, vector
+	matX:	Pointer to array of Struct Matrix X, vector
 
 */
 
 	int size = matA[0].size;
 	int elements = size*size;
+
 	double AddG[elements];
 	for(int i =0; i<elements; i++) AddG[i] = 0;
+	double tempG[elements];
+	for(int i =0; i<elements; i++) tempG[i] = 0;
 
 	double AddB[elements];
 	for(int i =0; i<elements; i++) AddB[i] = 0;
+	double tempB[elements];
+	for(int i =0; i<elements; i++) tempB[i] = 0;
 
-	#pragma omp prallel for
+//	#pragma omp prallel for
 	for (int i=0; i<nMat; i++){
 
 		printf("\n #%d ", i);
 		printf("%s\n", "Matrix Before Inversion");
 		print_matrix(matA[i].size, matA[i].matrix);
 
-		// B = inv(A)*C*B
-		// G = inv(A)*C*G
-		matrix_inv_m(matA[i].size, matA[i].matrix, matC[i].matrix, matB[i].matrix, matG[i].matrix);
-		// AddG = AddG + G
-		matrix_add(matA[i].size, AddG, matG[i].matrix, 1.0);
-		// AddB = AddB + B
-		matrix_add(matA[i].size, AddB, matB[i].matrix, 1.0);
+		// tempB = C*inv(A)*B
+		// tempG = C*inv(A)*G
+		matrix_inv_m(matA[i].size, matA[i].matrix, matC[i].matrix, matB[i].matrix, matG[i].matrix, tempB, tempG);
+
+		// AddG = AddG + tempG	:This is vector operation
+		matrix_add(matA[i].size, AddG, tempG, 1.0);
+		// AddB = AddB + tempB	:This is matrix operation
+		matrix_add(matA[i].size, AddB, tempB, 1.0);
 
 		printf("#%d ", i);
 		printf("%s\n", "Matrix After Inversion");
-		print_matrix(matG[i].size, matG[i].matrix);
+		print_matrix(matG[i].size, tempG);
 
 	}
 
-	printf("%s\n", "AddG Matrix WorkSpace After Addition");
-	print_matrix(size, AddG);
+//	printf("%s\n", "AddG Matrix WorkSpace After Addition");
+//	print_matrix(size, AddG);
 
 	printf("%s\n", "AddB Matrix WorkSpace After Addition");
 	print_matrix(size, AddB);
@@ -176,7 +197,7 @@ void solve_bbd( int 	nMat,
 	printf("%s\n", "matG[nMat]");
 	print_matrix(size, matG[nMat].matrix);
 
-	matrix_add(size, matG[nMat].matrix, AddG, -1.0);
+	matrix_add(size, matG[nMat].matrix, AddG, -1.0);	// this should be vector addition
 	matrix_add(size, matA[nMat].matrix, AddB, -1.0);
 
 	printf("%s\n", "After sub matA[nMat]");
@@ -185,10 +206,20 @@ void solve_bbd( int 	nMat,
 	printf("%s\n", "After sub matG[nMat]");
 	print_matrix(size, matG[nMat].matrix);
 
-	matrix_invm(size, matA[nMat].matrix, matG[nMat].matrix, AddG);
+	matrix_invm(size, matA[nMat].matrix, matG[nMat].matrix, matX[nMat].matrix);
 
-	printf("%s\n", "After final Inversion matA[nMat]");
-	print_matrix(size, AddG);
+	printf("%s\n", "After final Inversion matX[nMat]");
+	print_matrix(size, matX[nMat].matrix);
+
+	for (int i=0; i<nMat; i++){
+
+		matrix_xi(size, matA[i].matrix, matB[i].matrix, matG[i].matrix, matX[nMat].matrix, matX[i].matrix);
+		printf("%s\n", "-------------------------------------------------" );
+		printf("%d th ",i);
+		printf("%s\n", "Solution Xi Matrices");
+		print_matrix(size, matX[i].matrix);
+
+	}
 
 }
 
@@ -199,6 +230,7 @@ int main() {
 	struct Matrix matB[nMat];
 	struct Matrix matC[nMat];
 	struct Matrix matG[nMat+1];
+	struct Matrix matX[nMat+1];
 
 	int n; // temp
 
@@ -245,7 +277,7 @@ int main() {
 		}
 	}
 
-	solve_bbd(nMat, matA, matB, matC, matG);
+	solve_bbd(nMat, matA, matB, matC, matG, matX);
 
 /*
 	#pragma omp prallel for
@@ -291,7 +323,7 @@ int main() {
 	matrix_inv(N2, M2);
 	print_matrix(N2,M2);
 */
-	printf("\n %s\n", "End End End End End End End End End End End End" );
+	printf("\n %s\n", "-------------------------------------------------" );
     return 0;   
 }
 
